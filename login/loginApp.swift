3468ae8e03e756f0e696cc75ad323e5a3ae815a6
Rudraptl16 <patelrudra99098@gmail.com>
2025-12-02 12:31:33 +0530

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by student on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
